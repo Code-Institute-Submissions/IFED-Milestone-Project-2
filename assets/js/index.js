6939d12607de6df75f32e9e2e785476cc4e8f5e2
CosmicAsad <asy_7@hotmail.co.uk>
2021-01-01 15:55:40 +0000

    Play modal now opens on button click

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,7 @@
-// Modal - select play button
-const modal = document.querySelector(".modal");
-const playbutton = document.querySelector("#playbutton");
-const closebutton = document.querySelector(".close-button")
+$('#playbutton').click(function() {
+    $('#playModal').modal('show')
+})
 
-function toggleModal() {
-    modal.classList.toggle("modal-active");
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
-
-playbutton.addEventListener("click", toggleModal);
-closebutton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick)
-
-//Main game
-
-//Timer
-
-$()
-
-// Select cards
-const cards = document.querySelector(".memory-card");
-
-function cardTurn() {
-    console.log('Yay!')
-    console.log(this)
-}
-
-cards.forEach(card => card.addEventListener('click', cardTurn));
+$('.close-button').click(function () {
+    $('#playModal').modal('hide')
+})
